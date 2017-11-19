b8669709fcdbaa7c1fdef55242769ac2af102f88
maurocastro <maurobarbosacastro@gmail.com>
2017-11-19 19:28:20 +0000

    Factorial with Extension Method, using C# Language

---
+++ ConsoleFactorial/ConsoleFactorial/MathFactorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class MathFactorial
+    {
+        public static int Factorial(this int value)
+        {
+            return (value <= 0) ? 1 : (value * Factorial(value - 1));
+        }
+        public static Int64 Factorial(this Int64 value)
+        {
+            return (value <= 0) ? 1 : (value * Factorial(value - 1));
+        }
+        public static ulong Factorial(this ulong value)
+        {
+            return (value <= 0) ? 1 : (value * Factorial(value - 1));
+        }
+        public static double Factorial(this double value)
+        {
+            return (value <= 0) ? 1 : (value * Factorial(value - 1));
+        }
+    }
+}
---
+++ ConsoleFactorial/ConsoleFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64[] arr = new Int64[] {
+                1,2,3,4,5,10,15,20,21,21,22,25,50,70,100,450,1000,5000
+            };
+            foreach (double i in arr)
+            {
+                Console.Out.WriteLine(String.Format("{0}! => {1}", i, i.Factorial().ToString()));
+            }
+            Console.ReadLine();
+        }
+    }
+}
